6bfe116889821f24ffd440ea6a4fa99822442a42
jennifer j <jenniferjuarezjj@gmail.com>
2024-02-02 13:01:32 -0500

    Initial Commit

---
+++ swift_learning_guide/swift_learning_guideApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_learning_guideApp.swift
+//  swift_learning_guide
+//
+//  Created by Jennifer Juarez on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_learning_guideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
